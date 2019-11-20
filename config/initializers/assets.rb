bf3d5010152e936c18f62299f715a76560ae6f08
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-11-20 14:48:16 -0700

    Added new styling to show page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w(application.js)
 Rails.application.config.assets.precompile += %w( blogposts.css )
+Rails.application.config.assets.precompile += %w( blogposts_show.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
