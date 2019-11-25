4ab34ee27121e437644d877ce3ef7c1042b85cff
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-11-25 11:49:59 -0700

    Moved styling to a file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.precompile += %w(application.js)
 Rails.application.config.assets.precompile += %w( blogposts.css )
 Rails.application.config.assets.precompile += %w( blogposts_show.css )
 Rails.application.config.assets.precompile += %w( actiontext.css )
+Rails.application.config.assets.precompile += %w( home.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
