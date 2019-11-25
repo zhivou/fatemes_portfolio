a2fc1ee50693511121f4a71cda383b712b1b23c8
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-11-25 16:34:01 -0700

    Added SEO to all pages, still need add more with client

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
              path: '',
              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  resources :publications
   resources :researches
   resources :blogposts
   root to: 'home#index'
