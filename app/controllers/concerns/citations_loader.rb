fd0f9e40d49168d35cb172f63cb7e3b237d9cd15
zhivou <zivou4@gmail.com>
2019-12-03 21:05:07 -0700

    Added laoder and writter to Scolar

--- app/controllers/concerns/citations_loader.rb
+++ app/controllers/concerns/citations_loader.rb
@@ -8,16 +8,20 @@ module CitationsLoader
   def load_citations
     # Add if statement to load the updates only each 12 hours
     # Scholar.first.publications[5][:title]
-    publications = Google::Scholar::Helper::Base.new(@google_scholar['link'])
-    publications_db = Scholar.new(
-        publications:publications.publications,
-        title: publications.title,
-        key_words: publications.key_words,
-        total_citations: publications.total_citations,
-        h_index: publications.h_index
-    )
-    if publications_db.save
-      puts 'SAVED!'
+    if !Scholar.exists? || Scholar.order(id: :desc).first.created_at + 12.hours < Time.now.utc
+      publications = Google::Scholar::Helper::Base.new(@google_scholar['link'])
+      publications_db = Scholar.new(
+          publications:publications.publications,
+          title: publications.title,
+          key_words: publications.key_words,
+          total_citations: publications.total_citations,
+          h_index: publications.h_index
+      )
+      if publications_db.save
+        puts 'New Google Scholars info was just uploaded.'
+      else
+        puts 'WARNING: Something went wrong. Check Google Scholar Helper Gem and Scholar web page'
+      end
     end
   end
 end
